d05177fbe1688360d27fc4f11e20b1f5f16ad0e0
lucasrj109 <lucasrj109@outlook.com>
2025-09-24 21:44:22 -0400

    Add jobseeker and recruiter signups

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class JobSeekerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=255, blank=True)
+    skills = models.TextField(blank=True)
+    education = models.TextField(blank=True)
+    work_experience = models.TextField(blank=True)
+    links = models.URLField(blank=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+
+class RecruiterProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name} (Recruiter)"
