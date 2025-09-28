7046168881f194e2f2ebbf40a937756e409f4633
ntran306 <nathangsu306@gmail.com>
2025-09-27 23:12:06 -0400

    Got individual jobs page to work

--- GTJobSearch/urls.py
+++ GTJobSearch/urls.py
@@ -5,6 +5,8 @@ URL configuration for GTJobSearch project.
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
+from django.conf import settings
+from django.conf.urls.static import static
 
 def jobseeker_home(request):
     return HttpResponse("<h1>Jobseeker Homepage (placeholder)</h1>")
@@ -21,3 +23,5 @@ urlpatterns = [
     path("recruiter/home", recruiter_home, name="recruiter_home"),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
