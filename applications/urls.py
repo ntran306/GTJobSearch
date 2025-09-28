51d98b287369d5d7b0b305fbc1e3c4a9260d5087
Kaylee Henry <kayleehenry162@gmail.com>
2025-09-28 17:45:59 -0400

    add job tracker and job status page

--- applications/urls.py
+++ applications/urls.py
@@ -6,4 +6,6 @@ app_name = "applications"
 urlpatterns = [
     path("apply/<int:job_id>/", views.apply_to_job, name="apply_to_job"),
     path("", views.view_applications, name="view_applications"),
+    path("update/<int:application_id>/", views.update_application_status, name="update_status"),
 ]
+
