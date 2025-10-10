e03ca1bbfc48d4a0ae74b568ef07d6e5c9cb5c65
Kaylee Henry <kayleehenry162@gmail.com>
2025-10-09 21:20:27 -0400

    resolved merge conflict in settings.py and updated styles

--- applications/urls.py
+++ applications/urls.py
@@ -6,6 +6,7 @@ app_name = 'applications'
 urlpatterns = [
     path('', views.view_applications, name='view_applications'),
     path('update/<int:application_id>/', views.update_application_status, name='update_status'),
-    path('apply/<int:job_id>/', views.apply_to_job, name='apply_for_job'),  # <-- Make sure this exists
+    path('apply/<int:job_id>/', views.apply_to_job, name='apply_to_job'),
+  # <-- Make sure this exists
 ]
 
