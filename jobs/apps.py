7cee1d029dd2269ad7e31b061b7926aef0185c69
remii5 <alesandercampbell1@gmail.com>
2025-10-17 17:20:42 -0400

    front end for browsing candidates

--- jobs/apps.py
+++ jobs/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from django.db.utils import OperationalError, ProgrammingError
 
 class JobsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
@@ -6,8 +7,8 @@ class JobsConfig(AppConfig):
 
     def ready(self):
         from .models import Skill
-        default_skills = [
-            "Python", "JavaScript", "C#", "React", "Django", "SQL", "HTML", "CSS"
-        ]
-        for skill_name in default_skills:
-            Skill.objects.get_or_create(name=skill_name)
+        try:
+            for s in ["Python", "C++", "React", "SQL"]:
+                Skill.objects.get_or_create(name=s)
+        except (OperationalError, ProgrammingError):
+            pass
